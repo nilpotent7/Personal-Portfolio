bc21123b09c896b90dca0f112a6b21d86e211340
TheUnknown <behrozkhan480@gmail.com>
2025-03-11 01:31:02 +0500

    Added Projects to Portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -12,15 +12,31 @@ export default function RaffleCaseStudy() {
 
                 <div className="projectTypeGrid">
                     <Link href='' className='projectCard' style={{backgroundImage: "url('projects/TheJuicer2.png')"}}>
+                        <div className="cardOverlay">
+                            <h2>The Juicer 2</h2>
+                            <p>3D Shooter with stylized graphics & arcade gameplay</p>
+                        </div>
                     </Link>
 
                     <Link href='' className='projectCard' style={{backgroundImage: "url('projects/GummyJump.png')"}}>
+                        <div className="cardOverlay">
+                            <h2>Gummy Jump 2</h2>
+                            <p>2D Platformer with gritty graphics & precise movement</p>
+                        </div>
                     </Link>
 
                     <Link href='' className='projectCard' style={{backgroundImage: "url('projects/Chronostrike.png')"}}>
+                        <div className="cardOverlay">
+                            <h2>Chronostrike</h2>
+                            <p>3D Endless Shooter made in 7 days</p>
+                        </div>
                     </Link>
 
                     <Link href='' className='projectCard' style={{backgroundImage: "url('projects/GluedTogether.png')"}}>
+                        <div className="cardOverlay">
+                            <h2>Glued Together</h2>
+                            <p>Top Down Puzzler made with a team in 7 days</p>
+                        </div>
                     </Link>
                 </div>
             </section>
@@ -30,9 +46,17 @@ export default function RaffleCaseStudy() {
 
                 <div className="projectTypeGrid">
                     <Link href='' className='projectCard' style={{backgroundImage: "url('projects/GasStation.png')"}}>
+                        <div className="cardOverlay">
+                            <h2>Gas Station</h2>
+                            <p>The only gas station in the middle of vast wilderness</p>
+                        </div>
                     </Link>
 
-                    <Link href='' className='projectCardEnd' style={{backgroundImage: "url('projects/HauntedSewer.png')"}}>
+                    <Link href='' className='projectCard' style={{backgroundImage: "url('projects/HauntedSewer.png')"}}>
+                        <div className="cardOverlay">
+                            <h2>Haunted Sewer</h2>
+                            <p>Abandonded sewerage rumoured to be connected with dark rituals</p>
+                        </div>
                     </Link>
                 </div>
             </section>
@@ -41,10 +65,18 @@ export default function RaffleCaseStudy() {
                 <h1>Programming</h1>
 
                 <div className="projectTypeGrid">
-                    <Link href='' className='projectCard' style={{backgroundImage: "url('projects/GasStation.png')"}}>
+                    <Link href='' className='projectCard' style={{backgroundImage: "url('projects/NeuralNetwork.png')"}}>
+                        <div className="cardOverlay">
+                            <h2>Machine Learning</h2>
+                            <p>Neural Networks trained to classify handwritten digits & alphabets from typefaces</p>
+                        </div>
                     </Link>
 
-                    <Link href='' className='projectCardEnd' style={{backgroundImage: "url('projects/HauntedSewer.png')"}}>
+                    <Link href='' className='projectCard' style={{backgroundImage: "url('projects/Raytracer.png')"}}>
+                        <div className="cardOverlay">
+                            <h2>Raytracing</h2>
+                            <p>Raytracer written as ComputeShader in Unity</p>
+                        </div>
                     </Link>
                 </div>
             </section>
