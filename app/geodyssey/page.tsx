040755e29ed071049b093668feeaa62f91be92e4
Nilpotent <behrozkhan480@gmail.com>
2025-07-30 00:59:26 +0500

    Added Links & License + Updated Media

--- app/geodyssey/page.tsx
+++ app/geodyssey/page.tsx
@@ -1,5 +1,5 @@
 import '@/app/geodyssey/page.scss'
-import { Carousel } from '@/components/carousel'
+import Link from "next/link"
 import CustomButton from '@/components/customButton'
 
 export const metadata = {
@@ -37,7 +37,7 @@ export default function Geodyssey() {
 
         <section className='written'>
             <div className='sidebar'>
-                <h2 style={{paddingBottom: "0.5rem"}}>Factsheet</h2>
+                <h2 style={{paddingBottom: "1.8rem"}}>Factsheet</h2>
 
                 <h3>Official Game Title</h3>
                 <p>Geodyssey</p>
@@ -68,8 +68,8 @@ export default function Geodyssey() {
                 <h3>Marketing Tagline</h3>
                 <p>Experience the Perplexing Impossible</p>
 
-                <h3>Website</h3>
-                <p>nilpotent.vercel.app/geodyssey</p>
+                {/* <h3>Website</h3>
+                <p>nilpotent.vercel.app/geodyssey</p> */}
 
                 <h3>Features</h3>
                 <ul>
@@ -121,38 +121,38 @@ export default function Geodyssey() {
             <div className='heading'>
                 <h1>Logos & Assets</h1>
                 <CustomButton
-                    title='Download' href='' arrowSpace={1}
+                    title='Download' href='https://drive.google.com/file/d/1MLeogbPIPDFze2hhZCcaRkrCL8ypcP30/view?usp=sharing' arrowSpace={1}
                     border={2} primary='#FF8A00' secondary='#FF4E00' text='var(--text-color)'
                 />
             </div>
             <div className='assets'>
                 <img src='/projects/Geodyssey/Logo.png' alt='Geodyssey Logo'/>
                 <img src='/projects/Geodyssey/HeaderCapsule.png' alt='Geodyssey Header Image'/>
-            </div>            
+            </div>
         </section>
 
         <section className='screenshotSection'>
             <div className='heading'>
                 <h1>Screenshots</h1>
                 <CustomButton
-                    title='Download' href='' arrowSpace={1}
+                    title='Download' href='https://drive.google.com/file/d/1pDhHEwKD6ya3BjGeVRJIbXcIVmWthRc7/view?usp=sharing' arrowSpace={1}
                     border={2} primary='#FF8A00' secondary='#FF4E00' text='var(--text-color)'
                 />
             </div>
             <div className='screenshots'>
-                <img                
+                <img
                     src="/projects/Geodyssey/Screenshot1.png"
                     alt="In-game Screenshot"
                 />
-                <img                
+                <img
                     src="/projects/Geodyssey/Screenshot2.png"
                     alt="In-game Screenshot"
                 />
-                <img                
+                <img
                     src="/projects/Geodyssey/Screenshot3.png"
                     alt="In-game Screenshot"
                 />
-                <img                
+                <img
                     src="/projects/Geodyssey/Screenshot4.png"
                     alt="In-game Screenshot"
                 />
@@ -160,7 +160,34 @@ export default function Geodyssey() {
                     src="/projects/Geodyssey/Screenshot5.png"
                     alt="In-game Screenshot"
                 />
-            </div>            
+                <img
+                    src="/projects/Geodyssey/Screenshot6.png"
+                    alt="In-game Screenshot"
+                />
+                <img
+                    src="/projects/Geodyssey/Screenshot7.png"
+                    alt="In-game Screenshot"
+                />
+                <img
+                    src="/projects/Geodyssey/Screenshot8.png"
+                    alt="In-game Screenshot"
+                />
+            </div>
+        </section>
+
+        <section className='license'>
+            <div className='heading'>
+                <h1>License</h1>
+            </div>
+            <div className='licenseText'>
+                <p>All content on this page falls under <Link style={{ color: "#FF8A00"}} href='https://creativecommons.org/licenses/by/4.0/?ref=chooser-v1'>CC BY 4.0</Link></p>
+                <p>You are free to copy, redistribute, remix, transform, or build upon the material for any purpose, even commercially.</p>
+                <p>The licensor cannot revoke these freedoms as long as you follow the license terms.</p>
+                <h4>Under the following terms:</h4>
+                <p>You must give <Link style={{ color: "#FF8A00"}} href='https://creativecommons.org/licenses/by/4.0/?ref=chooser-v1#ref-appropriate-credit'>appropriate credit</Link> to this <Link style={{ color: "#FF8A00"}} href='https://nilpotent.vercel.app/geodyssey'>page</Link>.</p>
+                <p>You may indicate if changes were made.</p>
+                <p>You may do so in any reasonable manner.</p>
+            </div>
         </section>
     </>
 }
